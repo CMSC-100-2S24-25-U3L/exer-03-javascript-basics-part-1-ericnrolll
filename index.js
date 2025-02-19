42558d5d85e73c5f4382740175781badfa686960
ericnrolll <fdsindayen@up.edu.ph>
2025-02-19 11:56:46 +0800

    Added reversePassword() function

---
+++ index.js
@@ -0,0 +1,89 @@
+
+function validatePassword(password1, password2) {
+    if (password1 === password2) { // check if the two passwords are the same
+
+        if (password1.length >= 8) { // check if the password is at least 8 characters
+            
+            // check if the password has at least one number
+            for  (let a=0; a < password1.length; a++) {
+
+                if (isNumber(password1.charAt(a))) { // check if a character is from is equal to 1:9
+                    break;
+                } 
+
+                if (a == password1.length-1) { // return false after last loop (no numbers in the string)
+                    return false;
+                }
+            }
+
+            // check if the password has at least one uppercase character
+            for  (let a=0; a < password1.length; a++) {
+
+                if (isNumber((password1.charAt(a)))) {
+                    if (a == password1.length-1) { // return false after last loop (only numbers in the string)
+                        return false;
+                    }
+        
+                    continue;
+                }
+        
+                if ((password1.charAt(a)).toUpperCase() === password1.charAt(a)) {
+                    break; 
+                }
+        
+                if (a == password1.length-1) { // return false after last loop (no uppercase characters in the string)
+                    return false;
+                }
+            }
+
+            // check if the password has at least one lowercase character
+            for  (let a=0; a < password1.length; a++) {
+
+                if (isNumber((password1.charAt(a)))) {
+                    if (a == password1.length-1) { // return false after last loop (only numbers in the string)
+                        return false;
+                    }
+        
+                    continue;
+                }
+        
+                if ((password1.charAt(a)).toLowerCase() === password1.charAt(a)) {
+                    break; 
+                }
+        
+                if (a == password1.length-1) { // return false after last loop (no uppercase characters in the string)
+                    return false;
+                }
+            }
+
+            return true; // return true if all requirements are checked
+
+        } else {
+            return false; // password is not at least 8 characters
+    }
+    } else {
+        return false; // the two passwords are not the same
+    }
+};
+
+function isNumber(x) { // function that checks if a string is numerical
+    for (let b=0; b < 10; b++) { // check if a character is from is equal to 1:9
+        if (x == b) {
+            return true;
+        }
+    }
+
+    return false;
+}; 
+
+function reversePassword(password) {
+    var reversed = '';
+
+    for (let a=0; a<password.length; a++) {
+        reversed = reversed + password[password.length-a-1] 
+    }
+
+    return reversed;
+
+}
+
---
+++ try.js
@@ -0,0 +1,6 @@
+
+string0 = ''
+string1 = 'abcd'
+
+
+console.log(string0.length)
